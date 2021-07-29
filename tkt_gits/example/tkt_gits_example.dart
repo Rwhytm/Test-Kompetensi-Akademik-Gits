6c91b70e2871e48a1922402c98967e790e50ac2e
Rwhytm <rajuwahyudi842@gmail.com>
2021-07-30 00:38:05 +0700

    created project

--- tkt_gits/example/tkt_gits_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:tkt_gits/tkt_gits.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
--- tkt_gits/lib/tkt_gits.dart
+++
@@ -1,8 +0,0 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library tkt_gits;
-
-export 'src/tkt_gits_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
